344c2d4d451a2983f4bb5549fac5badada61d8b3
Tomasz Slabon <t.slabon@bioinfo.pl>
2020-01-07 12:32:27 +0100

    Added mutex locking when saving msg history

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1320,6 +1320,8 @@ bool CWallet::SaveMsgToHistory(
         return false;
     }
 
+    LOCK(cs_wallet);
+
     if (!WalletBatch(*msgDatabase).WriteMsgTxToHistory(hash, toAddress, subject, encrypted, time)) {
         LogPrintf("Failed to wrtie encrypted message to db\n");
         return false;
