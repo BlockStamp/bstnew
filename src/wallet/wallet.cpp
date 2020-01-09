156b2e05585a3d9c54867d1726daf54dda569534
michal-siek <michal-siek@github.com>
2020-01-09 09:48:05 +0100

    remove cancelled transaction from history and read tab

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1564,10 +1564,37 @@ void CWallet::TransactionAddedToMempool(const CTransactionRef& ptx) {
 }
 
 void CWallet::TransactionRemovedFromMempool(const CTransactionRef &ptx) {
+    using namespace internal_miner;
+
     LOCK(cs_wallet);
-    auto it = mapWallet.find(ptx->GetHash());
+
+    uint256 hash = ptx->GetHash();
+    auto it = mapWallet.find(hash);
     if (it != mapWallet.end()) {
         it->second.fInMempool = false;
+    }
+
+    if (ptx->IsMsgTx())
+    {
+        int tip = chainActive.Height();
+
+        const uint32_t minAcceptedHeight =
+            (tip > MSG_TXN_ACCEPTED_DEPTH) ? (tip-MSG_TXN_ACCEPTED_DEPTH) : 0;
+
+        const CTransaction& txn = *ptx;
+        ExtNonce extNonce{};
+        if (!readExtNonce(txn, extNonce) || extNonce.tip_block_height < minAcceptedHeight)
+        {
+            LogPrintf("Removing msg transaction from database: %s\n", ptx->GetHash().ToString());
+
+            encrMsgMapWallet.erase(hash);
+            WalletBatch(*msgDatabase).EraseEncrMsgTx(hash);
+            NotifyEncrMsgTransactionChanged(this);
+
+            encrMsgHistory.erase(hash);
+            WalletBatch(*msgDatabase).EraseMsgTxToHistory(hash);
+            NotifyMsgSent(this);
+        }
     }
 }
 
