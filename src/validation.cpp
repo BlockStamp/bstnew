b4005da354ab67dace3e51c5450f1abdbe42399a
Tomasz Slabon <t.slabon@bioinfo.pl>
2020-01-09 11:15:44 +0100

    Fee estimation: skipping msg txns fee

--- src/validation.cpp
+++ src/validation.cpp
@@ -1010,7 +1010,8 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
         // - it's not being re-added during a reorg which bypasses typical mempool fee limits
         // - the node is not behind
         // - the transaction is not dependent on any other transactions in the mempool
-        bool validForFeeEstimation = !fReplacementTransaction && !bypass_limits && IsCurrentForFeeEstimation() && pool.HasNoInputsOf(tx) && !modulo::ver_2::isMakeBetTx(tx);
+        bool validForFeeEstimation = !fReplacementTransaction && !bypass_limits && IsCurrentForFeeEstimation() && pool.HasNoInputsOf(tx)
+                && !modulo::ver_2::isMakeBetTx(tx) && !tx.IsMsgTx();
 
         // Store transaction in memory
         pool.addUnchecked(entry, setAncestors, validForFeeEstimation);
